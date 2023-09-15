4268f73adf85fe661b022267c15b098e3bbd60fb
Suhail P A <Suhailabbas23@gmail.com>
2023-09-15 10:02:03 +0530

    Added BookMark and Search Screen

---
+++ app/src/main/java/com/example/newsapp/ui/theme/bookmarks/BookMarkScreen.kt
@@ -0,0 +1,22 @@
+package com.example.newsapp.ui.theme.bookmarks
+
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BookMarkScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "BookMark Screen", style = MaterialTheme.typography.titleLarge)
+    }
+}
